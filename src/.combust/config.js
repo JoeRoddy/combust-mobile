de2a1e0198c65e3dd14ad5a0503b86ddb46af746
Joe <mrjoeroddy@gmail.com>
2018-03-19 14:25:25 -0400

    remove firebase config

--- src/.combust/config.js
+++ src/.combust/config.js
@@ -1,8 +1,1 @@
-export const firebaseConfig = {
-  "apiKey": "AIzaSyDoNnUO4i5MYFYnf_mG5RZLQMobLV-43ik",
-  "databaseURL": "https://not-a-secret-agency.firebaseio.com",
-  "storageBucket": "not-a-secret-agency.appspot.com",
-  "authDomain": "not-a-secret-agency.firebaseapp.com",
-  "messagingSenderId": "23967090475",
-  "projectId": "not-a-secret-agency"
-}
+export const firebaseConfig = null;
