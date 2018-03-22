1fe735faf286ecf529d8e63b62e4bb7ea3876445
Joe <mrjoeroddy@gmail.com>
2018-03-22 15:35:50 -0400

    userlist is now scrollable

--- src/components/users/UserList.js
+++ src/components/users/UserList.js
@@ -1,38 +1,39 @@
 import React from "react";
 import { observer } from "mobx-react";
-import { View } from "react-native";
+import { View, ScrollView } from "react-native";
 import { List, ListItem } from "react-native-elements";
 
 export default (UserList = observer(({ users, onUserClicked }) => {
   return (
-    <List>
-      {users &&
-        Object.keys(users).map((userId, i) => {
-          const user = users[userId];
-          if (!user) {
-            return <View />;
-          }
-          return (
-            <ListItem
-              key={i}
-              avatar={{ uri: user.iconUrl }}
-              roundAvatar
-              title={user.displayName}
-              onPress={() => onUserClicked(user)}
-              underlayColor="#edeff2"
-              rightIcon={
-                <View
-                  style={{
-                    width: 15,
-                    height: 15,
-                    borderRadius: 15 / 2,
-                    backgroundColor: user.isOnline ? "#67f391" : "gray"
-                  }}
-                />
-              }
-            />
-          );
-        })}
-    </List>
+    <ScrollView>
+      <List containerStyle={{ marginTop: 0 }}>
+        {users &&
+          Object.keys(users).map((userId, i) => {
+            const user = users[userId];
+            return user ? (
+              <ListItem
+                key={i}
+                avatar={{ uri: user.iconUrl }}
+                roundAvatar
+                title={user.displayName}
+                onPress={() => onUserClicked(user)}
+                underlayColor="#edeff2"
+                rightIcon={
+                  <View
+                    style={{
+                      width: 15,
+                      height: 15,
+                      borderRadius: 15 / 2,
+                      backgroundColor: user.isOnline ? "#67f391" : "gray"
+                    }}
+                  />
+                }
+              />
+            ) : (
+              <View />
+            );
+          })}
+      </List>
+    </ScrollView>
   );
 }));
