aad9a34e4187996b672d09fa401e6d5be514c67e
Joe Roddy <mrjoeroddy@gmail.com>
2018-06-05 16:06:11 -0400

    apply observer to user action bar, wasnt updating w/stores

--- src/.combust/config.js
+++ src/.combust/config.js
@@ -1,1 +1,8 @@
-export const firebaseConfig = null;
+export const firebaseConfig = {
+  "apiKey": "AIzaSyDHMJ-UsNlXd0S-yF3lFZ8o4w2x8IB3CU8",
+  "databaseURL": "https://combusttests.firebaseio.com",
+  "storageBucket": "combusttests.appspot.com",
+  "authDomain": "combusttests.firebaseapp.com",
+  "messagingSenderId": "652709282584",
+  "projectId": "combusttests"
+}
