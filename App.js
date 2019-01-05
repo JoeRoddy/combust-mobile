8479ffe85c88afc26a4e4278ef27623401881df6
Joe <mrjoeroddy@gmail.com>
2019-01-05 15:40:33 -0500

    cp -r from react-native init

--- App.js
+++ App.js
@@ -1,25 +1,41 @@
-import React from "react";
-import firebase from "firebase/app";
+import React, { Component } from "react";
+import { Platform, StyleSheet, Text, View } from "react-native";
 
-import firebaseConfig from "./src/.combust/firebase.config.json";
-import { initializeStores } from "./src/.combust/init";
-import nav from "./src/helpers/NavigatorHelper";
-import Routes from "./src/components/Routes";
+const instructions = Platform.select({
+  ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
+  android:
+    "Double tap R on your keyboard to reload,\n" +
+    "Shake or press menu button for dev menu"
+});
 
-console.ignoredYellowBox = ["Setting a timer"]; // swallow react native + firebase warning
-firebaseConfig &&
-  firebaseConfig.projectId &&
-  firebase.initializeApp(firebaseConfig);
-initializeStores();
-
-export default class App extends React.Component {
+type Props = {};
+export default class App extends Component<Props> {
   render() {
     return (
-      <Routes
-        ref={navigatorRef => {
-          nav.setContainer(navigatorRef);
-        }}
-      />
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.instructions}>To get started, edit App.js</Text>
+        <Text style={styles.instructions}>{instructions}</Text>
+      </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF"
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: "center",
+    margin: 10
+  },
+  instructions: {
+    textAlign: "center",
+    color: "#333333",
+    marginBottom: 5
+  }
+});
