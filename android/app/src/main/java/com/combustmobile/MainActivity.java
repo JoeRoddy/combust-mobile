440b53faeec252158d1b81846cdc1994d7b9558d
Joe <mrjoeroddy@gmail.com>
2019-08-03 12:54:27 -0400

    merge

--- android/app/src/main/java/com/combustmobile/MainActivity.java
+++ android/app/src/main/java/com/combustmobile/MainActivity.java
@@ -1,4 +1,4 @@
-package com.rninit;
+package com.combustmobile;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
      */
     @Override
     protected String getMainComponentName() {
-        return "rnInit";
+        return "combustMobile";
     }
 }
